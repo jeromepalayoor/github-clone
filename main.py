91b9e2ffb8f3796f84b98d4aec9d6a80401fcab8
R Mukilan <rmukilan115@gmail.com>
2024-04-09 22:14:08 +0800

    added css shit

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+import sqlite3
+
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET', 'POST'])
+def index():
+    username='jeromepalayoor'
+    return render_template('index.html',username=username)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
+@app.route('/user')
+@app.route('/users')
+def users():
+    return render_template('username.html')
+
+@app.route('/user/<username>', methods = ['GET', 'POST'])
+@app.route('/users/<username>', methods = ['GET', 'POST'])
+def username_route(username):
+    return render_template('username.html', username=username)
+
+app.run()
